4dbc96f106c01c393c3be22340ce3fdf1cd0154b
vlnx <vlnx@mail.com>
2018-01-28 02:21:15 -0600

    Let gapplication handle options
    TODO: correct
    g_variant_dict_lookup_value: assertion 'ensure_valid_dict (dict)' failed

--- luakit.c
+++ luakit.c
@@ -105,90 +105,6 @@ glib_log_writer(GLogLevelFlags log_level_flags, const GLogField *fields, gsize n
 }
 #endif
 
-/* load command line options into luakit and return uris to load */
-static gchar **
-parseopts(int *argc, gchar *argv[], gboolean **nonblock, gboolean **nonunique)
-{
-    GOptionContext *context;
-    gboolean *version_only = NULL;
-    gboolean *check_only = NULL;
-    gchar **uris = NULL;
-    globalconf.profile = NULL;
-    gboolean verbose = FALSE;
-    gchar *log_lvl = NULL;
-
-    /* save luakit exec path */
-    globalconf.execpath = g_strdup(argv[0]);
-
-    /* define command line options */
-    const GOptionEntry entries[] = {
-        { "check",     'k', 0, G_OPTION_ARG_NONE,         &check_only,          "check config and exit",     NULL   },
-        { "config",    'c', 0, G_OPTION_ARG_STRING,       &globalconf.confpath, "configuration file to use", "FILE" },
-        { "profile",   'p', 0, G_OPTION_ARG_STRING,       &globalconf.profile,  "profile name to use",       "NAME" },
-        { "nonblock",  'n', 0, G_OPTION_ARG_NONE,         nonblock,             "run in background",         NULL   },
-        { "nonunique", 'U', 0, G_OPTION_ARG_NONE,         nonunique, "Use the nonunique gapplication flag",  NULL   },
-        { "uri",       'u', 0, G_OPTION_ARG_STRING_ARRAY, &uris,                "uri(s) to load at startup", "URI"  },
-        { "verbose",   'v', 0, G_OPTION_ARG_NONE,         &verbose,             "print verbose output",      NULL   },
-        { "log",       'l', 0, G_OPTION_ARG_STRING,       &log_lvl,             "specify precise log level", "NAME" },
-        { "version",   'V', 0, G_OPTION_ARG_NONE,         &version_only,        "print version and exit",    NULL   },
-        { NULL,        0,   0, 0,                         NULL,                 NULL,                        NULL   },
-    };
-
-    /* Save a copy of argv */
-    globalconf.argv = g_ptr_array_new_with_free_func(g_free);
-    for (gint i = 0; i < *argc; ++i)
-        g_ptr_array_add(globalconf.argv, g_strdup(argv[i]));
-
-    /* parse command line options */
-    context = g_option_context_new("[URI...]");
-    g_option_context_add_main_entries(context, entries, NULL);
-    g_option_context_add_group(context, gtk_get_option_group(FALSE));
-    g_option_context_parse(context, argc, &argv, NULL);
-    g_option_context_free(context);
-
-    /* Trim unparsed arguments off copy of argv */
-    for (gint i = 0; i < *argc; ++i) {
-        while ((unsigned)i < globalconf.argv->len && !strcmp(g_ptr_array_index(globalconf.argv, i), argv[i]))
-            g_ptr_array_remove_index(globalconf.argv, i);
-    }
-
-    /* print version and exit */
-    if (version_only) {
-        g_printf("luakit %s\n", VERSION);
-        exit(EXIT_SUCCESS);
-    }
-
-    if (!log_lvl)
-        log_set_verbosity("all", verbose ? LOG_LEVEL_verbose : LOG_LEVEL_info);
-    else {
-        log_set_verbosity("all", LOG_LEVEL_info);
-        parse_log_level_option(log_lvl);
-        if (verbose)
-            warn("invalid mix of -v and -l, ignoring -v...");
-    }
-
-    /* check config syntax and exit */
-    if (check_only) {
-        init_directories();
-        luaH_init(NULL);
-        if (!luaH_parserc(globalconf.confpath, FALSE)) {
-            g_fprintf(stderr, "Confiuration file syntax error.\n");
-            exit(EXIT_FAILURE);
-        } else {
-            g_fprintf(stderr, "Configuration file syntax OK.\n");
-            exit(EXIT_SUCCESS);
-        }
-    }
-
-    if (uris && argv[1])
-        fatal("invalid mix of -u and default uri arguments");
-
-    if (uris)
-        return uris;
-    else
-        return g_strdupv(argv + 1);
-}
-
 void
 luakit_startup(GApplication UNUSED(app))
 {
@@ -205,27 +121,64 @@ luakit_startup(GApplication UNUSED(app))
         fatal("couldn't find rc file");
 }
 
+// Let g_application_real_local_command_line call activate/open on remaining arguments
+// 'activate' is called when there aren't any arguments to call 'open' on, here, send an empty open call
 void
-luakit_options(GApplication *app, GApplicationCommandLine *cmdline, gpointer UNUSED(user_data))
+luakit_activate(GApplication *app)
 {
-    return;
+    GFile **files;
+    gint n_files = 0;
+    files = g_new(GFile *, n_files);
+    g_application_open(G_APPLICATION(app), files, n_files, "");
+    g_free(files);
 }
 
-void
-luakit_local_options(GApplication *app, GVariantDict *dict, gpointer UNUSED(user_data))
+gint
+luakit_local_options(GApplication UNUSED(app), GVariantDict *dict)
 {
-    return;
+    GVariant *value;
+    /* print version and exit */
+    value = g_variant_dict_lookup_value(dict, "version", NULL);
+    if (value) {
+        g_printf("luakit %s\n", VERSION);
+        return 0;
+    }
+    /* check config syntax and exit */
+    value = g_variant_dict_lookup_value(dict, "check", NULL);
+    if (value) {
+        init_directories();
+        luaH_init(NULL);
+        if (!luaH_parserc(globalconf.confpath, FALSE)) {
+            g_fprintf(stderr, "Confiuration file syntax error.\n");
+            return 1;
+        } else {
+            g_fprintf(stderr, "Configuration file syntax OK.\n");
+            return 0;
+        }
+    }
+    /* log level */
+    gboolean verbose = false;
+    value = g_variant_dict_lookup_value(dict, "verbose", NULL);
+    if (value) verbose = true;
+    gchar *log_lvl = NULL;
+    if (g_variant_dict_lookup(dict, "log", "s", &log_lvl)) {
+        log_set_verbosity("all", LOG_LEVEL_info);
+        parse_log_level_option(log_lvl);
+        if (verbose)
+            warn("invalid mix of -v and -l, ignoring -v...");
+    } else {
+        log_set_verbosity("all", verbose ? LOG_LEVEL_verbose : LOG_LEVEL_info);
+    }
+    return -1;
 }
 
 gint
 main(gint argc, gchar *argv[])
 {
+    globalconf.starttime = l_time();
     globalconf.application_name = "org.luakit";
     if (!g_application_id_is_valid(globalconf.application_name))
         fatal("invalid application name");
-    gboolean *nonblock = NULL;
-    gboolean *nonunique = false;
-    globalconf.starttime = l_time();
 
     log_init();
 
@@ -235,48 +188,20 @@ main(gint argc, gchar *argv[])
     setlocale(LC_ALL, "");
     setlocale(LC_NUMERIC, "C");
 
-    /* parse command line opts and get uris to load */
-    gchar **uris = parseopts(&argc, argv, &nonblock, &nonunique);
-
-    globalconf.application = gtk_application_new(globalconf.application_name,
-                                                 nonunique ? G_APPLICATION_NON_UNIQUE | G_APPLICATION_HANDLES_COMMAND_LINE | G_APPLICATION_HANDLES_OPEN
-                                                 : G_APPLICATION_HANDLES_COMMAND_LINE | G_APPLICATION_HANDLES_OPEN);
-
-    g_signal_connect(globalconf.application, "command-line", G_CALLBACK(luakit_options), NULL);
-    g_signal_connect(globalconf.application, "handle-local-options", G_CALLBACK(luakit_local_options), NULL);
-    g_signal_connect(globalconf.application, "startup", G_CALLBACK(luakit_startup), NULL);
-    g_signal_connect(globalconf.application, "open", G_CALLBACK(luakit_browse), NULL);
-
-    // register gapplication, lets probe to find an existing instance work
-    GError *error = NULL;
-    g_application_register(G_APPLICATION(globalconf.application), NULL, &error);
-    if (error != NULL
-        && g_application_get_is_registered(G_APPLICATION(globalconf.application))) {
-        g_error_free(error);
-        g_object_unref(G_OBJECT(globalconf.application));
-        globalconf.application = NULL;
-        fatal("unable to register gapplication: %s\n", error->message);
-    }
-
-    // translate **uris array to GFiles
-    GFile **files;
-    gint n_files;
-    gint i;
-    n_files = g_strv_length(uris);
-    files = g_new(GFile *, n_files);
-    for (i = 0; i < n_files; i++)
-        files[i] = g_file_new_for_commandline_arg(uris[i]);
-
-    // if there's a secondary, send GFiles and exit
-    if (g_application_get_is_remote(G_APPLICATION(globalconf.application))) {
-        g_application_open(G_APPLICATION(globalconf.application), files, n_files, "");
-        return 0;
-    }
-
-    /* hide command line parameters so process lists don't leak (possibly
-       confidential) URLs */
-    for (gint i = 1; i < argc; i++)
-        memset(argv[i], 0, strlen(argv[i]));
+    // Before gapplication kicks in, test for existence of the following flags
+    gboolean *nonblock = NULL;
+    gboolean *nonunique = false;
+    /* const GOptionEntry low_lvl_entries[] = { */
+    /*     { "nonblock",  'n', 0, G_OPTION_ARG_NONE, &nonblock,  "run in background",                    NULL }, */
+    /*     { "nonunique", 'U', 0, G_OPTION_ARG_NONE, &nonunique, "Use the nonunique gapplication flag",  NULL }, */
+    /*     { NULL } */
+    /* }; */
+    /* GOptionContext *context; */
+    /* context = g_option_context_new(NULL); */
+    /* g_option_context_add_main_entries(context, low_lvl_entries, NULL); */
+    /* g_option_context_set_help_enabled(context, false); */
+    /* g_option_context_parse(context, &argc, &argv, NULL); */
+    /* g_option_context_free(context); */
 
     /* if non block mode - respawn, detach and continue in child */
     if (nonblock) {
@@ -292,15 +217,44 @@ main(gint argc, gchar *argv[])
         }
     }
 
-    // if primary, send initial open calls
-    if (!g_application_get_is_remote(G_APPLICATION(globalconf.application))) {
-        g_application_open(G_APPLICATION(globalconf.application), files, n_files, "");
-    }
-    for (i = 0; i < n_files; i++)
-        g_object_unref(files[i]);
-    g_free (files);
+    /* save luakit exec path */
+    globalconf.execpath = g_strdup(argv[0]);
+    /* Save a copy of argv */
+    globalconf.argv = g_ptr_array_new_with_free_func(g_free);
+    for (gint i = 0; i < argc; ++i)
+        g_ptr_array_add(globalconf.argv, g_strdup(argv[i]));
 
-    int status = g_application_run(G_APPLICATION(globalconf.application), argc, argv);
+    globalconf.application = gtk_application_new(globalconf.application_name, G_APPLICATION_HANDLES_OPEN);
+    if (nonunique)
+        g_application_set_flags(G_APPLICATION(globalconf.application), G_APPLICATION_NON_UNIQUE | G_APPLICATION_HANDLES_OPEN);
+
+    /* register command line options */
+    const GOptionEntry entries[] = {
+        { "check",     'k', 0, G_OPTION_ARG_NONE,   NULL,                 "check config and exit",               NULL   },
+        { "version",   'V', 0, G_OPTION_ARG_NONE,   NULL,                 "print version and exit",              NULL   },
+        { "verbose",   'v', 0, G_OPTION_ARG_NONE,   NULL,                 "print verbose output",                NULL   },
+        { "log",       'l', 0, G_OPTION_ARG_STRING, NULL,                 "specify precise log level",           "NAME" },
+        { "config",    'c', 0, G_OPTION_ARG_STRING, &globalconf.confpath, "configuration file to use",           "FILE" },
+        { "profile",   'p', 0, G_OPTION_ARG_STRING, &globalconf.profile,  "profile name to use",                 "NAME" },
+        // following two entries are only in this entry group to be displayed via --help
+        { "nonblock",  'n', 0, G_OPTION_ARG_NONE,   &nonblock,            "run in background",                   NULL   },
+        { "nonunique", 'U', 0, G_OPTION_ARG_NONE,   &nonunique,           "Use the nonunique gapplication flag", NULL   },
+        { NULL }
+    };
+    g_application_add_main_option_entries(G_APPLICATION(globalconf.application), entries);
+
+    g_signal_connect(globalconf.application, "handle-local-options", G_CALLBACK(luakit_local_options), NULL);
+    g_signal_connect(globalconf.application, "startup", G_CALLBACK(luakit_startup), NULL);
+    g_signal_connect(globalconf.application, "open", G_CALLBACK(luakit_browse), NULL);
+    g_signal_connect(globalconf.application, "activate", G_CALLBACK(luakit_activate), NULL);
+
+    // Duplicate argv for g_application_run, then erase the real argv
+    char **argv2 = g_strdupv(argv);
+    /* hide command line parameters so process lists don't leak (possibly
+       confidential) URLs */
+    for (gint i = 1; i < argc; i++)
+        memset(argv[i], 0, strlen(argv[i]));
+    int status = g_application_run(G_APPLICATION(globalconf.application), argc, argv2);
     g_object_unref(globalconf.application);
     return status;
 }
